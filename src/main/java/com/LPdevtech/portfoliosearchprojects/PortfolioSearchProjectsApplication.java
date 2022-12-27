f1524ed95bb3d40e3f22857b20a76914e7afaed3
Eduardo-LP <eduardolemospedroso@gmail.com>
2022-12-27 16:46:50 -0300

    Envia projeto inicial

---
+++ src/main/java/com/LPdevtech/portfoliosearchprojects/PortfolioSearchProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.LPdevtech.portfoliosearchprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSearchProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioSearchProjectsApplication.class, args);
+	}
+
+}
